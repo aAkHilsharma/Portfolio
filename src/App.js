667bd49f71151d061ee6d40c1bdccbe6eb47a8d3
aAkHilsharma <akhilsharma6721@gmail.com>
2023-08-31 19:04:25 +0530

    added in skills

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Banner from "./components/Banner";
 import NavBar from "./components/Navbar";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
